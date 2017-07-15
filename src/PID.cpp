462fd88b801445f7f48e84a00f089b0e7dbf66fb
Sanjay <sshukla@nvidia.com>
2017-07-15 11:09:11 +0530

    basic pid implementation done

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,29 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+	this->Kp = Kp_;
+	this->Ki = Ki_;
+	this->Kd = Kd_;
+
+	//initlizing errors
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error =0.0;
+
+
 }
 
 void PID::UpdateError(double cte) {
+	d_error = cte - p_error;
+	p_error = cte;
+	i_error += cte;
+
 }
 
 double PID::TotalError() {
+	double total_error = 0.0 ;
+	total_error = - Kp*p_error  - Kd*d_error - Ki*i_error;
+	return total_error;
 }
 
